554a520b143c58686ed3edb06f7f46b8af1dd71d
adarsh <lifoxctn@gmail.com>
2025-08-14 01:30:24 +0530

    task completed

---
+++ nloop3.js
@@ -0,0 +1,16 @@
+// * # * #
+// * # * #
+// * # * #
+// * # * #
+
+for (row = 1; row <= 4; row++) {
+  str = "";
+  for (col = 1; col<= 4; col++) {
+    if (row % 2 == 0) {
+      str += "*";
+    } else {
+      str += "#";
+    }
+  }
+  console.log(str);
+}
