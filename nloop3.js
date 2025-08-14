436d52655599d70019d2d15c17893a106215d24a
adarsh <lifoxctn@gmail.com>
2025-08-14 09:54:22 +0530

    row to col

--- nloop3.js
+++ nloop3.js
@@ -6,7 +6,7 @@
 for (row = 1; row <= 4; row++) {
   str = "";
   for (col = 1; col<= 4; col++) {
-    if (row % 2 == 0) {
+    if (col % 2 == 0) {
       str += "*";
     } else {
       str += "#";
